d243116e23dc9ae21da61c6b3fb1ca2ea05d3603
Jack Seigerman <133878087+jackSeigerman@users.noreply.github.com>
2025-04-25 13:08:52 -0500

    Update populate_tables.py

--- populate_tables.py
+++ populate_tables.py
@@ -16,37 +16,37 @@ def main():
     #cur.execute("DELETE FROM enroll")
 
     with open("users.csv", 'r') as file:
-         with cur.copy(f"COPY courses FROM STDIN WITH (FORMAT CSV, HEADER true)") as copy:
+         with cur.copy(f"COPY users FROM STDIN WITH (FORMAT CSV, HEADER true)") as copy:
             copy.write(file.read())
     conn.commit()
 
     with open("isAdmin.csv", 'r') as file:
-        with cur.copy(f"COPY students FROM STDIN WITH (FORMAT CSV, HEADER true)") as copy:
+        with cur.copy(f"COPY isAdmin FROM STDIN WITH (FORMAT CSV, HEADER true)") as copy:
             copy.write(file.read())
     conn.commit()
 
     with open("isWorker.csv", 'r') as file:
-        with cur.copy(f"COPY enroll FROM STDIN WITH (FORMAT CSV, HEADER true)") as copy:
+        with cur.copy(f"COPY isWorker FROM STDIN WITH (FORMAT CSV, HEADER true)") as copy:
             copy.write(file.read())
     conn.commit()
 
     with open("tickets.csv", 'r') as file:
-         with cur.copy(f"COPY courses FROM STDIN WITH (FORMAT CSV, HEADER true)") as copy:
+         with cur.copy(f"COPY tickets FROM STDIN WITH (FORMAT CSV, HEADER true)") as copy:
             copy.write(file.read())
     conn.commit()
 
     with open("ticketStatus.csv.csv", 'r') as file:
-         with cur.copy(f"COPY courses FROM STDIN WITH (FORMAT CSV, HEADER true)") as copy:
+         with cur.copy(f"COPY ticketStatus FROM STDIN WITH (FORMAT CSV, HEADER true)") as copy:
             copy.write(file.read())
     conn.commit()
 
     with open("reports.csv", 'r') as file:
-         with cur.copy(f"COPY courses FROM STDIN WITH (FORMAT CSV, HEADER true)") as copy:
+         with cur.copy(f"COPY reports FROM STDIN WITH (FORMAT CSV, HEADER true)") as copy:
             copy.write(file.read())
     conn.commit()
 
     with open("assigns.csv.csv", 'r') as file:
-         with cur.copy(f"COPY courses FROM STDIN WITH (FORMAT CSV, HEADER true)") as copy:
+         with cur.copy(f"COPY assigns FROM STDIN WITH (FORMAT CSV, HEADER true)") as copy:
             copy.write(file.read())
     conn.commit()
 
