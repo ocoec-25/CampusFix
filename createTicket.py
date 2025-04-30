5b6237fc210ab3966791372a87580005dc7cc1b5
Osoinach <osoja-26@rhodes.edu>
2025-04-30 16:12:39 -0500

    working on ticket

--- createTicket.py
+++ createTicket.py
@@ -1,1 +1,20 @@
-#todo
+#TODO
+from nicegui import ui, app
+def ticket(cur):
+    username = app.storage.user.get("username", None)
+    userid = app.storage.user.get("userid", None)
+
+    if username is None:
+        return ui.navigate.to('/login?redirect_url=/admin')
+
+
+
+
+#add function that finds the most recent ticket number and adds one
+#TODO
+
+
+
+def insert_ticket(cur):
+    cur.execute("""Insert
+    """)
