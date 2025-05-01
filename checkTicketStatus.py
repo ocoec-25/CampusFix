6fad4ce864ff903e926f66df5259c70211ad8599
ocoec-25 <143973868+ocoec-25@users.noreply.github.com>
2025-05-01 02:55:50 -0500

    Update checkTicketStatus.py

--- checkTicketStatus.py
+++ checkTicketStatus.py
@@ -2,6 +2,11 @@ from nicegui import ui, app
 #TODO
 #user or admin can delete ticket and check status
 
+def ticket_details(tid, cur):
+    print("showing detail")
+    pass
+
+
 def status(cur):
 
     username = app.storage.user.get('username', None)
@@ -15,9 +20,10 @@ def status(cur):
         return ui.navigate.to('/login?redirect_url=/admin')
 
     cur.execute("""
-        SELECT t.tid, t.subject, r.reporterid
+        SELECT t.tid, t.subject, t.location, t.description, status, r.reporterid
         FROM tickets t
         JOIN reports r ON t.tid=r.tid 
+        JOIN ticketstatus s ON t.tid=s.tid
         WHERE r.reporterid=%s
     """, [userid])
     tickets = cur.fetchall()
@@ -25,13 +31,26 @@ def status(cur):
     columns = [
          {'name': 'tid', 'label': 'ID', 'field': 'tid', 'sortable': True},
          {'name': 'subject', 'label': 'Subject', 'field': 'subject', 'sortable': True},
+         {'name': 'location', 'label': 'Location', 'field': 'location', 'sortable': True},
+         {'name': 'description', 'label': 'Description', 'field': 'description', 'sortable': True},
+         {'name': 'status', 'label': 'Status', 'field': 'status', 'sortable': True},
          {'name': 'reporterid', 'label': 'Reporter ID', 'field': 'reporterid', 'sortable': True},
     ]
 
-    rows = [{'tid': t['tid'], 'subject': t['subject'], 'reporterid': t['reporterid']} for t in tickets]
+    rows = [{
+        'tid': t['tid'],
+        'subject': t['subject'],
+        'location': t['location'],
+        'description': t['description'],
+        'status': t['status'],
+        'reporterid': t['reporterid']
+    } for t in tickets]
 
     with ui.card().classes('mt-6'):
         ui.label("My Tickets").classes('text-xl font-bold')
-        ui.table(columns=columns, rows=rows).classes('w-full')
+        table = ui.table(columns=columns, rows=rows, row_key='tid', selection='single', on_select = lambda e: click_ticket(t)).classes('w-full')
+
+
+        ui.button('View Ticket', on_click=lambda: process_step2())
 
 
