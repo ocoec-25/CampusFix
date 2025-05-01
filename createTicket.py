a05c498a342fa3bc6a2a4eb58a9499446c94763c
Osoinach <osoja-26@rhodes.edu>
2025-04-30 20:36:21 -0500

    continued working on the ticket

--- createTicket.py
+++ createTicket.py
@@ -2,19 +2,31 @@
 from nicegui import ui, app
 def ticket(cur):
     username = app.storage.user.get("username", None)
-    userid = app.storage.user.get("userid", None)
-
     if username is None:
         return ui.navigate.to('/login?redirect_url=/admin')
 
+    def insert_reports(cur):
+        new_tid = get_new_tid(cur)
+        user_id = app.storage.user.get("userid", None)
+        cur.execute("INSERT INTO reports (reporterid, tid) VALUES (%d, %d)", [user_id, new_tid])
 
-
+    def insert_ticket(cur):
+        cur.execute("")
 
 #add function that finds the most recent ticket number and adds one
 #TODO
+def get_new_tid(cur):
+    cur.execute("SELECT MAX(tid) from reports")
+    new_tid = cur.fetchall
+    new_tid += 1
+    return new_tid
 
-
+"""
+def insert_reports(cur):
+    new_tid = get_new_tid(cur)
+    user_id = app.storage.user.get("userid", None)
+    cur.execute("INSERT INTO reports (reporterid, tid) VALUES (%d, %d)", [user_id, new_tid])
 
 def insert_ticket(cur):
-    cur.execute("""
-    """)
+    cur.execute("")
+"""
