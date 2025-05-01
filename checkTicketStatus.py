5453b17603468efa76ad4525eff60b45fa8f058e
ocoec-25 <143973868+ocoec-25@users.noreply.github.com>
2025-05-01 09:14:50 -0500

    Merge branch 'main' of https://github.com/ocoec-25/PythonProject1

--- checkTicketStatus.py
+++ checkTicketStatus.py
@@ -12,15 +12,13 @@ def status(cur):
     username = app.storage.user.get('username', None)
     userid = app.storage.user.get('user_id', None)
     is_user_admin = app.storage.user.get('is_admin', False)
-    #cur.execute("SELECT userid FROM users where email=%s", [username])
-    #userid = cur.fetchone()
-    #print(userid[0])
+
     #takes them back to home screen
     if username is None:
         return ui.navigate.to('/login?redirect_url=/admin')
 
     cur.execute("""
-        SELECT t.tid, t.subject, t.location, t.description, status, r.reporterid
+        SELECT t.tid, t.subject, t.location, t.description, status
         FROM tickets t
         JOIN reports r ON t.tid=r.tid 
         JOIN ticketstatus s ON t.tid=s.tid
@@ -29,12 +27,12 @@ def status(cur):
     tickets = cur.fetchall()
 
     columns = [
-         {'name': 'tid', 'label': 'ID', 'field': 'tid', 'sortable': True},
+         {'name': 'tid', 'label': 'Ticket ID', 'field': 'tid', 'sortable': True},
          {'name': 'subject', 'label': 'Subject', 'field': 'subject', 'sortable': True},
          {'name': 'location', 'label': 'Location', 'field': 'location', 'sortable': True},
          {'name': 'description', 'label': 'Description', 'field': 'description', 'sortable': True},
          {'name': 'status', 'label': 'Status', 'field': 'status', 'sortable': True},
-         {'name': 'reporterid', 'label': 'Reporter ID', 'field': 'reporterid', 'sortable': True},
+         #{'name': 'reporterid', 'label': 'Reporter ID', 'field': 'reporterid', 'sortable': True},
     ]
 
     rows = [{
@@ -43,7 +41,7 @@ def status(cur):
         'location': t['location'],
         'description': t['description'],
         'status': t['status'],
-        'reporterid': t['reporterid']
+        #'reporterid': t['reporterid']
     } for t in tickets]
 
     with ui.card().classes('mt-6'):
@@ -51,6 +49,6 @@ def status(cur):
         table = ui.table(columns=columns, rows=rows, row_key='tid', selection='single', on_select = lambda e: click_ticket(t)).classes('w-full')
 
 
-        ui.button('View Ticket', on_click=lambda: process_step2())
+        ui.button('View Ticket', on_click=lambda: process_step1())
 
 
