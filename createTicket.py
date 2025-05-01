3c28dbdcffedf1922d207f73188f8a9dc866e626
Osoinach <osoja-26@rhodes.edu>
2025-05-01 09:06:59 -0500

    yuh

--- createTicket.py
+++ createTicket.py
@@ -10,14 +10,13 @@ def ticket(cur):
         new_tid = get_new_tid(cur)
         user_id = app.storage.user.get("userid", None)
         cur.execute("INSERT INTO reports (reporterid, tid) VALUES (%d, %d)", [user_id, new_tid])
+        insert_ticket(cur)
 
     def insert_ticket(cur):
+        new_tid = get_new_tid(cur)
         cur.execute("")
+        return
 
-    def went_wrong(cur):
-        ui.button("Something went wrong. Please make sure all fields are filled in. Press this to reenter")
-        step1_card.set_visibility(True)
-        step2_card.set_visibility(False)
 
     with ui.card() as step1_card:
         with ui.row():
@@ -25,27 +24,16 @@ def ticket(cur):
             location_input = ui.input(label = "Location", placeholder = "Start Typing").classes("w-64")
             description_input = ui.input(label = "Description", placeholder = "Start Typing").classes("w-64")
             priority_input = ui.select(['1', '2', '3', '4', '5'],value = "1", label="Priority").classes("w-64")
-        ui.button("Submit Ticket", on_click=lambda: step2_card.set_visibility(True))
+        ui.button("Submit Ticket", on_click=lambda: insert_reports(cur))
 
 
-    with ui.card() as step2_card:
-        step1_card.set_visibility(False)
-        if subject_input == '':
-            went_wrong(cur)
-        elif location_input == '':
-            went_wrong(cur)
-        elif description_input == '':
-            went_wrong(cur)
-        else:
-            insert_reports(cur)
-            insert_ticket(cur)
 
 #add function that finds the most recent ticket number and adds one
 #TODO
 def get_new_tid(cur):
     cur.execute("SELECT MAX(tid) from reports")
     new_tid = cur.fetchall
-    new_tid += 1
+    new_tid = new_tid + 1
     return new_tid
 
 """
