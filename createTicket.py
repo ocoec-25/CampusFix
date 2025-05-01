87ae0c3477fbc4960b59ef2871ef17b67fcdb248
Osoinach <osoja-26@rhodes.edu>
2025-04-30 22:52:17 -0500

    ticket stuff

--- createTicket.py
+++ createTicket.py
@@ -5,6 +5,13 @@ def ticket(cur):
     if username is None:
         return ui.navigate.to('/login?redirect_url=/admin')
 
+    with ui.card() as ticket_card:
+        with ui.row().classes('items-center'):
+            ui.label('Subject:')
+            student_id_box = ui.input()
+
+
+
     def insert_reports(cur):
         new_tid = get_new_tid(cur)
         user_id = app.storage.user.get("userid", None)
