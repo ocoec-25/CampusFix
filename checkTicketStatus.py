0fb017fffd68256c5e795af893b1734a78894f79
ocoec-25 <143973868+ocoec-25@users.noreply.github.com>
2025-05-01 02:09:46 -0500

    Update checkTicketStatus.py

--- checkTicketStatus.py
+++ checkTicketStatus.py
@@ -5,39 +5,33 @@ from nicegui import ui, app
 def status(cur):
 
     username = app.storage.user.get('username', None)
+    userid = app.storage.user.get('user_id', None)
     is_user_admin = app.storage.user.get('is_admin', False)
-
+    #cur.execute("SELECT userid FROM users where email=%s", [username])
+    #userid = cur.fetchone()
+    #print(userid[0])
     #takes them back to home screen
     if username is None:
         return ui.navigate.to('/login?redirect_url=/admin')
 
-    ui.label("My Tickets").classes('text-2xl font-bold mb-4')
+    cur.execute("""
+        SELECT t.tid, t.subject, r.reporterid
+        FROM tickets t
+        JOIN reports r ON t.tid=r.tid 
+        WHERE r.reporterid=%s
+    """, [userid])
+    tickets = cur.fetchall()
 
-    def show_user_tickets():
-        cur.execute("""
-            SELECT t.tid, t.subject, t.reporterid
-            FROM tickets t
-            JOIN reports r
-            WHERE t.tid = r.tid
-            and t.reporterid=%s"
-        """, [username])
-        tickets = cur.fetchall()
+    columns = [
+         {'name': 'tid', 'label': 'ID', 'field': 'tid', 'sortable': True},
+         {'name': 'subject', 'label': 'Subject', 'field': 'subject', 'sortable': True},
+         {'name': 'reporterid', 'label': 'Reporter ID', 'field': 'reporterid', 'sortable': True},
+    ]
 
-        with ui.card() as step1_card:
-            ui.label("All Tickets").classes('text-xl font-bold mt-4')
+    rows = [{'tid': t['tid'], 'subject': t['subject'], 'reporterid': t['reporterid']} for t in tickets]
 
-            columns = [
-                {'name': 'tid', 'label': 'ID', 'field': 'tid', 'sortable': True},
-                {'name': 'subject', 'label': 'Subject', 'field': 'subject', 'sortable': True},
-                {'name': 'reporterid', 'label': 'Reporter ID', 'field': 'reporterid', 'sortable': True},
-            ]
+    with ui.card().classes('mt-6'):
+        ui.label("My Tickets").classes('text-xl font-bold')
+        ui.table(columns=columns, rows=rows).classes('w-full')
 
-            with ui.table(columns=columns, rows=tickets, row_key='tid').classes('w-full') as table:
-                table.add_slot('body-cell-status', '''
-                        <q-td :props="props">
-                            <q-chip :color="props.value === 'open' ? 'green' : 'gray'">
-                                {{ props.value }}
-                            </q-chip>
-                        </q-td>
-                    ''')
 
