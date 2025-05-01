72d1c2f79f57a809a3970e06ee7bfaa815266ec5
ocoec-25 <143973868+ocoec-25@users.noreply.github.com>
2025-05-01 09:27:20 -0500

    Update checkTicketStatus.py

--- checkTicketStatus.py
+++ checkTicketStatus.py
@@ -48,6 +48,6 @@ def status(cur):
         ui.label("My Tickets").classes('text-xl font-bold')
         table = ui.table(columns=columns, rows=rows, row_key='tid', selection='single', on_select = lambda e: click_ticket(t)).classes('w-full')
 
-        ui.button('View Ticket', on_click=lambda: process_step1())
+        #ui.button('View Ticket', on_click=lambda: process_step1())
 
 
